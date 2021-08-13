c513a18cc169c873080561701938986aa56189a8
glen-s-abraham <glenprojects666@gmail.com>
2021-08-13 12:34:52 +0530

    feat:Filtering on tags recipes and ingredients

--- app/recipes/tests/test_ingredients_api.py
+++ app/recipes/tests/test_ingredients_api.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 from core.models import Ingredient
+from core.models import Recipe
 from recipes.serializers import IngredientSerializer
 
 
@@ -73,3 +74,41 @@ class PrivateIngredientApiTest(TestCase):
         """Test wether ingredients validations are performed"""
         res = self.client.post(INGREDIENTS_URL, {})
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_ingredients_assigned_to_recipe(self):
+        """Test filtering ingrredients by those assigned to recipes"""
+        ing1 = Ingredient.objects.create(user=self.user, name='Apples')
+        ing2 = Ingredient.objects.create(user=self.user, name='Turky')
+        recipe = Recipe.objects.create(
+            title='Apple crumble',
+            time_minutes=10,
+            price=5.00,
+            user=self.user
+        )
+        recipe.ingredients.add(ing1)
+        res = self.client.get(INGREDIENTS_URL, {'assigned_only': 1})
+        serializer1 = IngredientSerializer(ing1)
+        serializer2 = IngredientSerializer(ing2)
+        self.assertIn(serializer1.data, res.data)
+        self.assertNotIn(serializer2.data, res.data)
+
+    def test_retrieve_tag_assigned_unique(self):
+        """Test filtering tags assigned returns unique items"""
+        ingredient = Ingredient.objects.create(user=self.user, name="Chicken")
+        Ingredient.objects.create(user=self.user, name="Cucumber")
+        recipe1 = Recipe.objects.create(
+            title='chicken tikka',
+            time_minutes=10,
+            price=5.00,
+            user=self.user
+        )
+        recipe1.ingredients.add(ingredient)
+        recipe2 = Recipe.objects.create(
+            title='Chicken curry',
+            time_minutes=10,
+            price=5.00,
+            user=self.user
+        )
+        recipe2.ingredients.add(ingredient)
+        res = self.client.get(INGREDIENTS_URL, {'assigned_only': 1})
+        self.assertEqual(len(res.data), 1)
